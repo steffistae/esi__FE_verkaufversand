26d6251fe79b94a794785acbe7dec5217b225e9b
AlexSp <36576348+AlexSperka@users.noreply.github.com>
2020-06-12 19:43:32 +0200

    Download CSV File

--- src/prod/getCSV.js
+++ src/prod/getCSV.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import '../App.css';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import GlobalAppBar from '../components/GlobalAppBar';  
-import ProdAppBar from '../components/ProdAppBar';  
+import GlobalAppBar from '../components/GlobalAppBar';
+import ProdAppBar from '../components/ProdAppBar';
 
 import { FormControl } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
@@ -17,14 +17,12 @@ import axios from 'axios'
 
 class GetCSV extends Component {
     constructor(props) {
-        super(props)
-
+        super(props);
         this.state = {
-            prodOrderNr: '', //Nummer
-            statusID: '3', //ID des Kunden
-            statusdescription: 'Produktion abgeschlossen', //String mit Beschreibung
-            data: null
-        }
+            data: [],
+            name: this.props.filename ? this.props.filename : 'data',
+            url: ''
+        };
     }
 
     changeHandler = e => {
@@ -32,23 +30,29 @@ class GetCSV extends Component {
     }
 
     submitHandler = e => {
+        var test = {
+            "key1": "value1",
+            "key2": "value2",
+            "key3": "value3",
+        };
+
         e.preventDefault()
         console.log(this.state)
         this.setState(
             { newProd: true }
         )
         axios
-            .post('https://5club7wre8.execute-api.eu-central-1.amazonaws.com/sales/updatestatus', this.state)
+            .post('https://2pkivl4tnh.execute-api.eu-central-1.amazonaws.com/prod/sortOrders', { crossdomain: true })
             .then((res) => {
                 console.log(res.data)
                 var data = JSON.stringify(res.data)
                 data = JSON.parse(data)
-                data = data.body.message
+                data = data.url
                 return data
             })
             .then(data => {
                 console.log("data: " + data)
-                this.setState({ data: data })
+                this.setState({ url: data })
             })
             .catch(error => {
                 console.log(error)
@@ -70,8 +74,34 @@ class GetCSV extends Component {
                             <ProdAppBar />
                         </div>
 
-                        <div style={{ padding: '20px' }} ><h2>CSV File für die Maschinenkonfiguration </h2></div>
+                        <div style={{ padding: '20px' }} ><h2>CSV File erstellen und herunterladen </h2></div>
 
+                        <div style={{ width: '800px', padding: '20px' }}>
+                            <FormControl>
+                                <Grid container
+                                    direction="row"
+                                    justify="center"
+                                    alignItems="flex-start"
+                                >
+
+                                    <Grid
+                                        container spacing={3}>
+                                        <Grid item xs={6} sm={6}>
+                                            <Button type="submit" style={{ float: 'left', margin: '20px' }} color="primary" variant="contained">CSV-Datei erstellen</Button>
+                                        </Grid>
+                                        <Grid item xs={6} sm={6}>
+
+                                            <a href={this.state.url} target="_blank" rel="noopener noreferrer" download>
+                                                <Button style={{ float: 'left', margin: '20px' }} color="primary" variant="contained">
+                                                    <i className="fas fa-download" /> Download CSV-File  </Button>
+                                            </a>
+
+                                        </Grid>
+                                    </Grid >
+                                </Grid>
+
+                            </FormControl>
+                        </div>
                     </form>
                 </div>
             </>
