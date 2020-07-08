5510d7393546bc7daac2359db68169f1994ba126
steffistae <62403344+steffistae@users.noreply.github.com>
2020-07-08 16:18:50 +0200

    update materialnr dropdown
    dropdown

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -38,7 +38,17 @@
 
 
 const dropdown = 
-{10000001: '10000001 - T-Shirt', 10000002: '10000002 - Tasse'};
+{10000001: '10000001 - T-Shirt | weiß | XS', 
+10000002: '10000002 - T-Shirt | weiß | S',
+10000003: '10000003 - T-Shirt | weiß | M',
+10000004: '10000005 - T-Shirt | weiß | L',
+10000005: '10000005 - T-Shirt | weiß | XL',
+10000011: '10000001 - T-Shirt | schwarz | XS', 
+10000012: '10000002 - T-Shirt | schwarz | S',
+10000013: '10000003 - T-Shirt | schwarz | M',
+10000014: '10000005 - T-Shirt | schwarz | L',
+10000015: '10000005 - T-Shirt | schwarz | XL'
+};
 
 
  module.exports = { dropdown };
